f28bc8b5a455e45304ff1a89ea1a218aa8d8630e
saiyaka2000 <saiyakamondal1@gmail.com>
2022-06-15 21:20:15 +0530

    corporate website
    Build with Custom Boostrap

--- js/main.js
+++ js/main.js
@@ -97,6 +97,44 @@ window.onscroll = ()=>{
   });
   
   
+
+  //----------------------------brand----------------------
+
+  var swiper = new Swiper(".brand", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+
+  loop: true,
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+  });
+
+
   
 // darkmode
 var toggleSwitch = document.querySelector(".toggle-button");
